a593cd4a1d7a1be2bf2fd3aa92aa1502c608f983
Тихонов Никита <tn@smarthead.ru>
2020-10-27 11:55:27 +0300

    Layout fixes

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDebouncedFn } from 'beautiful-react-hooks';
 import { IPostInfo, IThumbnail } from '../../interfaces';
 import styles from './Post.module.css';
