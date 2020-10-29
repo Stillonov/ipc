7595b0da341380d6588af9625bc4ce0b887f92e1
Тихонов Никита <tn@smarthead.ru>
2020-10-29 15:34:00 +0300

    Fix post types

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -6,41 +6,12 @@ import styles from './Post.module.css';
 const FB_ID: string = process.env.REACT_APP_FB_ID || '';
 const FB_ACCESS_TOKEN: string = process.env.REACT_APP_FB_ACCESS_TOKEN || '';
 
-// const getPost = async (postUrl: string): Promise<IPostInfo> => {
-//     const response = await fetch(
-//         `https://graph.facebook.com/v8.0/instagram_oembed?url=${postUrl}&access_token=${FB_ID}|${FB_ACCESS_TOKEN}`
-//     );
-//
-//     if (response.ok) {
-//         return (await response.json()) as IPostInfo;
-//     } else {
-//         console.log(`Ошибка HTTP: ${response.status}`);
-//     }
-// };
-
-function api<T>(url: string): Promise<T> {
-    return fetch(url).then((response) => {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        return response.json<T>();
-    });
-}
-
-api<{ title: string; message: string }>('v1/posts/1')
-    .then(({ title, message }) => {
-        console.log(title, message);
-    })
-    .catch((error) => {
-        /* show error message */
-    });
-
 const Post: React.FC = () => {
     const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
     const [error, setError] = useState(false);
 
     const handleChange = useDebouncedFn(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
+        async (event: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
             const url = event.target.value;
 
             if (!url || !url.trim()) {
@@ -49,22 +20,30 @@ const Post: React.FC = () => {
                 return;
             }
 
-            getPost(url)
-                .then((postInfo) => {
-                    setError(false);
+            const response: Response = await fetch(
+                `https://graph.facebook.com/v8.0/instagram_oembed?url=${url}&access_token=${FB_ID}|${FB_ACCESS_TOKEN}`
+            );
 
-                    setThumbnail({
-                        width: postInfo.thumbnail_width,
-                        height: postInfo.thumbnail_height,
-                        url: postInfo.thumbnail_url,
-                    });
-                })
-                .catch(() => {
-                    setError(true);
+            if (response.ok) {
+                const data = (await response.json()) as IPostInfo;
+
+                setError(false);
+
+                setThumbnail({
+                    width: data.thumbnail_width,
+                    height: data.thumbnail_height,
+                    url: data.thumbnail_url,
                 });
+            } else {
+                setError(true);
+            }
         },
         300
     );
+
+    const handleFocus = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        event.target.select();
+    };
 
     return (
         <div className={styles.Post}>
@@ -76,6 +55,7 @@ const Post: React.FC = () => {
                     name="url"
                     placeholder="Insert the Instagram post link"
                     onChange={handleChange}
+                    onFocus={handleFocus}
                 />
 
                 {!error && thumbnail && thumbnail.width && thumbnail.height && (
